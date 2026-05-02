efd8a58c78ff9a493bfd0275073f0997a215f136
sakshishirole <shirolesakshi23@gmail.com>
2026-05-02 22:19:26 +0530

    PRODIGY_WD_04

---
+++ task3.js
@@ -0,0 +1,10 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    navbar.classList.toggle('active');
+};
+
+window.onscroll = () => {
+    navbar.classList.remove('active');
+};
